34b8a25468df2b2d9c00bd0850c64266170f35a9
tmarr1426 <tmarr1426@gmail.com>
2024-07-05 07:36:10 -0500

    Updates to new branch

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,9 +1,54 @@
 import { Box, Card, CardContent } from "@mui/material";
 
+import DriveTime from "../assets/Self/rainbow-drive-time.png";
+
 const Projects = ({ repos }) => {
   return (
     <div>
-      <div></div>
+      <div>
+        <h1>Live Projects:</h1>
+        <Card
+          style={{
+            width: "35em",
+            margin: "1em",
+            boxShadow: "4px 4px 4px #000000",
+            borderRadius: "1em",
+            backgroundColor: "#ECEAED",
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "row" }}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  src={DriveTime}
+                  alt="DriveTime Logo"
+                  style={{ height: "10em" }}
+                />
+                <a
+                  href="https://drivetime.netlify.app"
+                  style={{
+                    fontSize: 32,
+                    textDecoration: "underline 2px",
+                    fontWeight: "700",
+                    textAlign: "left",
+                  }}
+                  color="#494888"
+                  gutterBottom
+                >
+                  DriveTime Live
+                </a>
+              </Box>
+            </CardContent>
+          </Box>
+        </Card>
+      </div>
       <div>
         <h1>Github Repos:</h1>
         <h4>Github Username: tmarr1426</h4>

cebfc6ec52bf4f4c545b33a859d8d670d289c70e
tmarr1426 <tmarr1426@gmail.com>
2025-01-20 10:23:56 -0600

    updates

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -6,28 +6,28 @@ import ImageOne from "../assets/Miniatures/DSC_0484_result.png";
 import ImageTwo from "../assets/Miniatures/DSC_0486_result.png";
 import ImageThree from "../assets/Miniatures/DSC_0507_result.png";
 import ImageFour from "../assets/Miniatures/DSC_0531_result.jpg";
-// import ImageFive from "../assets/Miniatures/ukotoa.jpg";
-// import ImageSix from "../assets/Miniatures/DSC_0202.jpg";
-// import ImageSeven from "../assets/Miniatures/DSC_0207.jpg";
-// import ImageEight from "../assets/Miniatures/DSC_0219.jpg";
-// import ImageNine from "../assets/Miniatures/PXL_20240405_130929601.jpg";
-// import ImageTen from "../assets/Miniatures/PXL_20240624_011745559.jpg";
-// import ImageEleven from "../assets/Miniatures/PXL_20240720_024605978.jpg";
-// import ImageTwelve from "../assets/Miniatures/PXL_20240720_024612681.jpg";
+import ImageFive from "../assets/Miniatures/ukotoa.jpg";
+import ImageSix from "../assets/Miniatures/DSC_0202.jpg";
+import ImageSeven from "../assets/Miniatures/DSC_0207.jpg";
+import ImageEight from "../assets/Miniatures/DSC_0219.jpg";
+import ImageNine from "../assets/Miniatures/PXL_20240405_130929601.jpg";
+import ImageTen from "../assets/Miniatures/PXL_20240624_011745559.jpg";
+import ImageEleven from "../assets/Miniatures/PXL_20240720_024605978.jpg";
+import ImageTwelve from "../assets/Miniatures/PXL_20240720_024612681.jpg";
 
 const images = [
   ImageOne,
   ImageTwo,
   ImageThree,
   ImageFour,
-  // ImageFive,
-  // ImageSix,
-  // ImageSeven,
-  // ImageEight,
-  // ImageNine,
-  // ImageTen,
-  // ImageEleven,
-  // ImageTwelve,
+  ImageFive,
+  ImageSix,
+  ImageSeven,
+  ImageEight,
+  ImageNine,
+  ImageTen,
+  ImageEleven,
+  ImageTwelve,
 ];
 
 const Carousel = () => {
